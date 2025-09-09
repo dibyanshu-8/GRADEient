7e8c34854d17d632cbd09a28eac76fcfebbbbf43
Dibyanshu kar <dibyanshukar8@gmail.com>
2025-09-09 20:52:24 +0530

    data ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -28,6 +28,17 @@ class DataIngestion:
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
             
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+
             logging.info("Ingestion of the data is completed")
-        except:
-            pass
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path,
+                
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
+
+if __name__=="__main__":
+    obj=DataIngestion()
+    obj.initiate_data_ingestion()
