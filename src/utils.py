9f58978b7808dde4c51381efb777afeadebcefb4
Dibyanshu kar <dibyanshukar8@gmail.com>
2025-09-14 17:55:10 +0530

    half way to model train xD

--- src/utils.py
+++ src/utils.py
@@ -15,3 +15,10 @@ def save_object(file_path,obj):
             dill.dump(obj,file_obj)
     except Exception as e:
         raise CustomException(e,sys)
+    
+def evaluate_models(X,y,models):
+    try:
+        X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
+        
+        report={}
+        
