7652e295c977e1ef0e0793dce2f845a01c883d45
Dibyanshu kar <dibyanshukar8@gmail.com>
2025-09-14 17:28:58 +0530

    data transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,9 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass # Import the dataclass decorator for automatically generating special methods like __init__().
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 @dataclass # Use the dataclass decorator to automatically create a class for storing configuration data.
 class DataIngestionConfig:
     # Define the file path for the training data CSV, placing it in an 'artifacts' folder.
@@ -54,4 +57,7 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+    
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
