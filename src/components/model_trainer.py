0c4d988493475caa132ccbca3c627e8482a09f74
Dibyanshu kar <dibyanshukar8@gmail.com>
2025-09-15 23:33:03 +0530

    Hyperparameter training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -11,7 +11,7 @@ from sklearn.ensemble import (
 )
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
-from sklearn.neighbors import KNeighborsRegressor
+from sklearn.neighbors import KNeighborsRegressor 
 from sklearn.tree import DecisionTreeRegressor
 from xgboost import XGBRegressor
 from src.exception import CustomException
@@ -36,19 +36,51 @@ class ModelTrainer:
                 test_array[:,:-1],
                 test_array[:,-1],
             )
-            #creating dictionary of models we have used
+            # models dictionary
             models={
-                "Random Forest":RandomForestRegressor(),
-                "Decision Tree":DecisionTreeRegressor(),
-                "Gradient Boosting":GradientBoostingRegressor(),
-                "Linear Regression":LinearRegression(),
-                "K-Neighbors Classifier":KNeighborsRegressor(),
-                "XGBClassifier":XGBRegressor(),
-                "CatBoosting Classifier":CatBoostRegressor(verbose=False),
-                "Adaboost Classifier":AdaBoostRegressor(),
+                "Random Forest": RandomForestRegressor(),
+                "Decision Tree": DecisionTreeRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(),
+                "Linear Regression": LinearRegression(),
+                "K-Neighbors Regressor": KNeighborsRegressor(),
+                "XGBRegressor": XGBRegressor(), 
+                "CatBoosting Regressor": CatBoostRegressor(verbose=False), 
+                "AdaBoost Regressor": AdaBoostRegressor(), 
             }
             
-            model_report:dict=evaluate_model(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+            # params dictionary
+            params={
+                "Decision Tree":{
+                    'criterion':['squared_error','friedman_mse','absolute_error','poisson'],
+                },
+                "Random Forest":{
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "K-Neighbors Regressor":{ 
+                    'n_neighbors':[5,7,9,11],
+                },
+                "XGBRegressor":{ 
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators':[8,16,32,64,128,256],
+                },
+                "CatBoosting Regressor":{ 
+                    'depth':[6,8,10],
+                    'learning_rate':[0.01,0.05,0.1],
+                    'iterations':[30,50,100],
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    'n_estimators':[8,16,32,64,128,256],
+                }
+            }
+            
+            model_report:dict=evaluate_model(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models,param=params)
             
             #To get best model score from dictionary
             best_model_score=max(sorted(model_report.values()))
@@ -69,4 +101,3 @@ class ModelTrainer:
             return r2_square
         except Exception as e:
             raise CustomException(e,sys)
-            
