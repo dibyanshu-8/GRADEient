e835818f6605ac6a14b8f7d97831cbbce16425cf
Dibyanshu kar <dibyanshukar8@gmail.com>
2025-09-15 22:48:05 +0530

    model trainer done

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -16,7 +16,7 @@ from sklearn.tree import DecisionTreeRegressor
 from xgboost import XGBRegressor
 from src.exception import CustomException
 from src.logger import logging
-from src.utils import save_object
+from src.utils import save_object,evaluate_model
 
 #for every component we need to create config files
 @dataclass
@@ -27,7 +27,7 @@ class ModelTrainer:
     def __init__(self):
         self.model_trainer_config=ModelTrainerConfig()
         
-    def initiate_model_trainer(self,train_array,test_array,preprocessor_path):
+    def initiate_model_trainer(self,train_array,test_array):
         try:
             logging.info("splitting training and test input data")
             X_train,y_train,X_test,y_test=(
@@ -49,5 +49,24 @@ class ModelTrainer:
             }
             
             model_report:dict=evaluate_model(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
-        except:
-            pass
+            
+            #To get best model score from dictionary
+            best_model_score=max(sorted(model_report.values()))
+            #To get best model name from disctionary
+            best_model_name=list(model_report.keys())[list(model_report.values()).index(best_model_score)]
+            best_model=models[best_model_name]
+            
+            if best_model_score<0.6:
+                raise CustomException("No best model found")
+            logging.info(f"Best found model on both training & testing dataset")
+            
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+            predicted=best_model.predict(X_test)
+            r2_square=r2_score(y_test,predicted)
+            return r2_square
+        except Exception as e:
+            raise CustomException(e,sys)
+            
--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
+from sklearn.metrics import r2_score
 
 from src.exception import CustomException
 
@@ -16,9 +17,21 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_models(X,y,models):
-    try:
-        X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
-        
+def evaluate_model(X_train,y_train,X_test,y_test,models):
+    try:        
         report={}
+        
+        for i in range(len(list(models))):
+            model=list(models.values())[i] #to go through each & every model
+            model.fit(X_train,y_train) #train the model
+            y_train_pred=model.predict(X_train)
+            y_test_pred=model.predict(X_test)
+            train_model_score=r2_score(y_train,y_train_pred)
+            test_model_score=r2_score(y_test,y_test_pred)
+            report[list(models.keys())[i]]=test_model_score
+            
+        return report
+    except Exception as e:
+        raise CustomException(e,sys)
+    
         
